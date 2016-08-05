57d595421c6ee0a03f1f57c208d97407f8f5be25
Kseniya <kseniya.panasyuk@gmail.com>
2016-08-04 21:23:07 -0400

    added condition on file type

--- src/main/java/org/fiery/md/actions/PreviewMdFileActionGroup.java
+++ src/main/java/org/fiery/md/actions/PreviewMdFileActionGroup.java
@@ -4,7 +4,10 @@ import com.intellij.ide.browsers.WebBrowser;
 import com.intellij.ide.browsers.WebBrowserManager;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.ComputableActionGroup;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.util.CachedValueProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,10 +23,20 @@ public class PreviewMdFileActionGroup extends ComputableActionGroup {
     @Override
     protected CachedValueProvider<AnAction[]> createChildrenProvider(@NotNull ActionManager actionManager) {
         return () -> {
-            List<WebBrowser> browsers = WebBrowserManager.getInstance().getBrowsers();
+            List<WebBrowser> browsers = WebBrowserManager.getInstance().getActiveBrowsers();
             AnAction[] actions = browsers.stream().map(PreviewMdFileAction::new).toArray(AnAction[]::new);
 
             return CachedValueProvider.Result.create(actions, WebBrowserManager.getInstance());
         };
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        VirtualFile currentFile = DataKeys.VIRTUAL_FILE.getData(event.getDataContext());
+        boolean isMarkDownFile = currentFile != null && "md".equals(currentFile.getExtension());
+        WebBrowserManager browserManager = WebBrowserManager.getInstance();
+        event.getPresentation().setVisible(isMarkDownFile
+                                           && browserManager.isShowBrowserHover()
+                                           && !browserManager.getActiveBrowsers().isEmpty());
     }
 }
