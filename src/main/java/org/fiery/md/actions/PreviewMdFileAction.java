4a3ec3dcb45f1e2065bc0f9cfb6cdba68c236713
Kseniya <kseniya.panasyuk@gmail.com>
2016-08-05 23:05:13 -0400

    added actual preview implementation

--- src/main/java/org/fiery/md/actions/PreviewMdFileAction.java
+++ src/main/java/org/fiery/md/actions/PreviewMdFileAction.java
@@ -4,9 +4,16 @@ import com.intellij.ide.browsers.BrowserLauncher;
 import com.intellij.ide.browsers.WebBrowser;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.ArrayUtil;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.vfs.VfsUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
 
 public class PreviewMdFileAction extends AnAction {
 
@@ -18,11 +25,24 @@ public class PreviewMdFileAction extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent event) {
-        Project project = event.getData(PlatformDataKeys.PROJECT);
-        BrowserLauncher.getInstance()
-                       .browseUsingPath("http://www.markdowntutorial.com/lesson/1/", null, browser, project,
-                                        ArrayUtil.EMPTY_STRING_ARRAY);
+        Editor currentEditor = DataKeys.EDITOR.getData(event.getDataContext());
+        Document document = currentEditor == null ? null : currentEditor.getDocument();
+        if (document == null) {
+            return;
+        }
+        try {
+            File file = File.createTempFile("markdown", ".html");
 
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file),
+                                                                    Charset.forName("UTF-8"))) {
+                writer.write(new GitMarkdownHelper().toGitMarkdownHtml(document));
+            }
+
+            BrowserLauncher.getInstance()
+                           .browse(VfsUtil.toUri(file).toURL().toExternalForm(), browser);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
